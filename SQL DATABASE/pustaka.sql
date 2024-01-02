f91d0ba946c936791c296e948eb7a7aa1742708d
Aldhi1211 <aldhitanca888@gmail.com>
2024-01-02 16:16:20 +0700

    update database SQL

--- SQL DATABASE/pustaka.sql
+++ SQL DATABASE/pustaka.sql
@@ -1,34 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 05, 2023 at 02:32 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+SET NAMES utf8mb4;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `db_pustaka`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `buku`
---
-
+DROP TABLE IF EXISTS `buku`;
 CREATE TABLE `buku` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `judul_buku` varchar(128) NOT NULL,
   `id_kategori` int(11) NOT NULL,
   `pengarang` varchar(64) NOT NULL,
@@ -38,143 +18,55 @@ CREATE TABLE `buku` (
   `stok` int(11) NOT NULL,
   `dipinjam` int(11) NOT NULL,
   `dibooking` int(11) NOT NULL,
-  `image` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `buku`
---
+  `image` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `buku` (`id`, `judul_buku`, `id_kategori`, `pengarang`, `penerbit`, `tahun_terbit`, `isbn`, `stok`, `dipinjam`, `dibooking`, `image`) VALUES
-(6, 'Buku Agama', 7, 'Gracias Kevin', 'Penerbit Erlangga', '2019', '1221', 10, 0, 0, 'img1700981538.jpg');
+(6,	'Novel',	2,	'Aldhi Tanca',	'Yourkidlee',	'2019',	'1220',	20,	0,	0,	'img1704186459.jpeg');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `kategori`
---
-
+DROP TABLE IF EXISTS `kategori`;
 CREATE TABLE `kategori` (
-  `id_kategori` int(11) NOT NULL,
-  `nama_kategori` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `kategori`
---
+  `id_kategori` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_kategori`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
-(1, 'Komputer'),
-(2, 'Bahasa'),
-(3, 'Sains'),
-(4, 'Hobby'),
-(5, 'Komunikasi'),
-(6, 'Hukum'),
-(7, 'Agama'),
-(8, 'Populer'),
-(9, 'Komik'),
-(11, 'Tidak Populer');
+(1,	'Komputer'),
+(2,	'Bahasa'),
+(3,	'Sains'),
+(4,	'Hobby'),
+(5,	'Komunikasi'),
+(6,	'Hukum'),
+(7,	'Agama'),
+(8,	'Populer'),
+(9,	'Komik'),
+(11,	'Tidak Populer');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `role`
---
-
+DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
-  `id` int(11) NOT NULL,
-  `role` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `role`
---
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `role` (`id`, `role`) VALUES
-(1, 'administrator'),
-(2, 'member');
+(1,	'administrator'),
+(2,	'member');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `image` varchar(128) NOT NULL,
   `password` varchar(128) NOT NULL,
   `role_id` int(11) NOT NULL,
   `is_active` int(1) NOT NULL,
-  `tanggal_input` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user`
---
+  `tanggal_input` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `user` (`id`, `nama`, `email`, `image`, `password`, `role_id`, `is_active`, `tanggal_input`) VALUES
-(2, 'M Maulana Andi Alfachrizy', 'alfasnif@gmail.com', 'pro1701776409.jpg', '$2y$10$mxE5fzwxpazjOAlzELMNbehQRYDwqckM5/vA6e8pHLB.Kr6Gs/4/e', 2, 1, 1701775833);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `buku`
---
-ALTER TABLE `buku`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `kategori`
---
-ALTER TABLE `kategori`
-  ADD PRIMARY KEY (`id_kategori`);
-
---
--- Indexes for table `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `buku`
---
-ALTER TABLE `buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `kategori`
---
-ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `role`
---
-ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+(3,	'Aldhi',	'aldhitanca888@gmail.com',	'pro1704186337.png',	'$2y$10$l7e.KukLf.G00rGk6i.zQufQs20Y5Ge44QmPNYC/VKmO0sOgDSsLy',	1,	1,	1704186276);
